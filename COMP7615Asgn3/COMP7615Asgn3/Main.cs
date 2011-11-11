6812d7670dc8fb1ec50db1e22360ffff7d0e442a
Kei <shadowkakeru@gmail.com>
2011-11-11 15:28:30 -0800

    ugly flashlight =P

--- COMP7615Asgn3/COMP7615Asgn3/Main.cs
+++ COMP7615Asgn3/COMP7615Asgn3/Main.cs
@@ -34,6 +34,7 @@ namespace COMP7615Asgn3
         bool isFog;
         bool isDay;
         bool isClip;
+        bool isFlash;
 
         // Cube
         List<Cube> cubes;
@@ -46,16 +47,18 @@ namespace COMP7615Asgn3
         Vector3 cartmanPosition;
         Vector2 cartmanDirection;
         float cartmanAngle;
-        int cartmanFrames;
         int cartmanMoveFrames;
-        const int cartmanFrameDelay = 10;
 
         // Lighting
-        private Vector3 ambientDay = new Vector3(0.7f, 0.7f, 0.7f);
-        private Vector3 ambientNight = new Vector3(0.1f, 0.1f, 0.1f);
-        private Vector3 diffuseDay = new Vector3(0.9f, 0.9f, 0.7f);
-        private Vector3 diffuseNight = new Vector3(0.8f, 0.8f, 0.8f);
-        private Vector3 diffuseDirection = new Vector3(0.1f, -1f, 0.1f);
+        Vector3 ambientDay = new Vector3(0.7f, 0.7f, 0.7f);
+        Vector3 ambientNight = new Vector3(0.1f, 0.1f, 0.1f);
+        Vector3 diffuseDay = new Vector3(0.9f, 0.9f, 0.7f);
+        Vector3 diffuseNight = new Vector3(0.8f, 0.8f, 0.8f);
+        Vector3 diffuseDirection = new Vector3(0.1f, -1f, 0.1f);
+
+        // Flashlight
+        Texture2D flashTexture;
+        Vector2 flashPosition;
 
         KeyboardState previousKey;
 
@@ -111,6 +114,21 @@ namespace COMP7615Asgn3
             CreateMaze(cubeModel);
 
             ResetPosition();
+
+            // Flashlight
+            flashTexture = Content.Load<Texture2D>("Images/Flashlight");
+
+            Color[] data = new Color[flashTexture.Width * flashTexture.Height];
+
+            flashTexture.GetData<Color>(data);
+            for (int k = 0; k < data.Length; k++)
+            {
+                data[k].A = 127;
+            }
+            flashTexture.SetData<Color>(data);
+
+            flashPosition = new Vector2(this.Window.ClientBounds.Width / 2 - flashTexture.Width / 2,
+                                        this.Window.ClientBounds.Height / 2 - flashTexture.Height / 2);
             
             // Set up WVP Matrices
             world = Matrix.Identity;
@@ -216,8 +234,12 @@ namespace COMP7615Asgn3
                 isMap = !isMap;
 
             // Toggle Fog
-            if (ks.IsKeyDown(Keys.F) && previousKey.IsKeyUp(Keys.F))
+            if (ks.IsKeyDown(Keys.G) && previousKey.IsKeyUp(Keys.G))
                 isFog = !isFog;
+
+            // Toggle Flashlight
+            if (ks.IsKeyDown(Keys.F) && previousKey.IsKeyUp(Keys.F))
+                isFlash = !isFlash;
 
             // Toggle Day/Night
             if (ks.IsKeyDown(Keys.L) && previousKey.IsKeyUp(Keys.L))
@@ -338,26 +360,25 @@ namespace COMP7615Asgn3
 
         private void EnemyMovement()
         {
-            if (cartmanFrames % cartmanFrameDelay == 0)
-            {
-                if (cartmanMoveFrames <= 0)
-                {
-                    Random random = new Random();
-                    cartmanAngle = (float)random.NextDouble() * MathHelper.TwoPi;
+            int[,] cells = maze.Cells;
 
-                    cartmanMoveFrames = 20;
-                }
+            //if (cartmanMoveFrames <= 0)
+            //{
+            //    Random random = new Random();
+            //    cartmanAngle = (float)random.NextDouble() * MathHelper.TwoPi;
 
-                float xPart = (float)Math.Sin(cartmanAngle) * 0.05f;
-                float zPart = (float)Math.Cos(cartmanAngle) * 0.05f;
+            //    cartmanMoveFrames = 20;
+            //}
 
-                Vector2 displacement = TryMove(new Vector2(xPart, -zPart));
+            //float xPart = (float)Math.Sin(cartmanAngle) * 0.01f;
+            //float zPart = (float)Math.Cos(cartmanAngle) * 0.01f;
 
-                cartmanPosition.X -= displacement.X;
-                cartmanPosition.Z += displacement.Y;
-            }
+            //Vector2 displacement = TryMove(new Vector2(xPart, -zPart));
 
-            cartmanFrames++;
+            //cartmanPosition.X -= displacement.X;
+            //cartmanPosition.Z += displacement.Y;
+
+            cartmanMoveFrames--;
         }
 
         /// <summary>
@@ -378,7 +399,21 @@ namespace COMP7615Asgn3
             }
 
             // Render Cartman
-            DrawModel(cartmanModel, cartmanPosition, -(float)MathHelper.PiOver2, cartmanAngle, 0.1f);
+            DrawModel(cartmanModel, cartmanPosition, -(float)MathHelper.PiOver2, -cartmanAngle, 0.1f);
+
+            // Flashlight
+            if (isFlash)
+            {
+                spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive);
+
+                spriteBatch.Draw(flashTexture, flashPosition, Color.White);
+
+                spriteBatch.End();
+
+                // Reset States for Rendering
+                GraphicsDevice.BlendState = BlendState.Opaque;
+                GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            }
 
             if (isMap)
             {
@@ -445,6 +480,13 @@ namespace COMP7615Asgn3
                 }
                 mesh.Draw();
             }
+        }
+
+        private void Flashlight(SpriteBatch sb)
+        {
+            sb.Begin(SpriteSortMode.Immediate, BlendState.Additive);
+
+            sb.End();
         }
 
         private void CreateMaze(Model model)
